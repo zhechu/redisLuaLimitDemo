76f2cf3bdf33348f4d3aea936655061986a3416a
zhechu <ling-yu-wang@qq.com>
2023-11-11 00:52:05 +0800

    feature:补上漏提交

---
+++ redisLuaLimitDemo/src/main/java/com/zhechu/limit/RedisLuaLimitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zhechu.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLuaLimitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLuaLimitDemoApplication.class, args);
+	}
+
+}
---
+++ redisLuaLimitDemo/src/test/java/com/zhechu/limit/RedisLuaLimitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhechu.limit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLuaLimitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
